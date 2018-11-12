b204797085e9f8df410273958b82eb3b2f44edf2
Phil Molock <phmolock@microsoft.com>
2018-11-12 14:40:54 -0600

    Adjusted event JS on index.html

--- uet.js
+++ uet.js
@@ -1,7 +1,10 @@
 // JavaScript source code
 function UETPushResumeDownload() {
+    console.log('Hello I\'m running');
     window.uetq = window.uetq || [];
     console.log(window.uetq);
     window.uetq.push({ 'ec': 'Resume', 'ea': 'Download' });
     console.log(window.uetq);
 };
+
+document.getElementById('resumeDownload').onclick = function () { UETPushResumeDownload();};
