1f5f9d02687fd55fc7ba9eb9183b6d5a55156b39
Phil Molock <phmolock@microsoft.com>
2018-11-12 14:29:40 -0600

    Added UET custom events to index.html for resume download

---
+++ uet.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+function UETPushResumeDownload() {
+    window.uetq = window.uetq || [];
+    console.log(window.uetq);
+    window.uetq.push({ 'ec': 'Resume', 'ea': 'Download' });
+    console.log(window.uetq);
+};
